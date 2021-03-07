5f4613fb99e1a09fea49ea611ca8109e17180a9e
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-03-07 13:42:33 +0900

    適正体重アプリの完成

--- eval/weight1.php
+++ eval/weight1.php
@@ -3,9 +3,12 @@
 require_once '../functions.php';
 
 $height = $_POST['height'];
-$height = (float)escapeHtml($height);
+$weight = $_POST['weight'];
+$height = (float)$height;
+$weight = (float)$weight;
 
-if ($height === 0.0) {
+
+if ($height === 0.0 && $weight === 0.0) {
    echo '無効な入力値です';
    exit;
 }
@@ -15,4 +18,16 @@ if ($height > 3) {
     exit;
 }
 
-echo 'あなたの適性体重は'. $height * $height * 22 .'です';
+if ($weight > 200) {
+    echo '体重を正しく入力してください';
+    exit;
+}
+
+$expectWeight = $height * $height * 22;
+$isWeight = $expectWeight < $weight;
+$mark = $isWeight?'-': "+";
+$diff = abs($expectWeight - $weight);
+
+echo 'あなたの適正体重は'. $expectWeight. 'です';
+echo '適正体重まで'.$mark .$diff. 'です';
+
