273c53098d5ab70dcc0c9eec2a50491e82db1e05
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-03-14 12:10:21 +0900

    データの表示

--- dbApp/connect1.php
+++ dbApp/connect1.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '../functions.php';
 
 try {
     $user = 'phpuser';
@@ -10,7 +10,13 @@ try {
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
     ];
     $dbh = new PDO('mysql:host=localhost;dbname=sample_db',$user,$password,$opt);
-    var_dump($dbh);
+    $sql = 'select * from books';
+    $statement = $dbh->query($sql);
+
+    while ($row = $statement->fetch()) {
+        echo escapeHtml($row[0]).'<br/>';
+        echo escapeHtml($row[1]);
+    }
 
 } catch (PDOException $e){
     echo $e->getMessage();
