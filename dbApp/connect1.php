0c18d514fe27fac08d35dfcc00e4c98e4b684f4d
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-03-14 11:57:32 +0900

    エラー処理の記述

--- dbApp/connect1.php
+++ dbApp/connect1.php
@@ -1,11 +1,18 @@
 <?php
 
-$user = 'phpuser';
-$password = 'dmadmadmadmaD1';
-$opt = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
-];
-$dbh = new PDO('mysql:host=localhost;dbname=sample_db',$user,$password,$opt);
-var_dump($dbh);
+
+try {
+    $user = 'phpuser';
+    $password = 'dmadmadmadmaD1';
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    ];
+    $dbh = new PDO('mysql:host=localhost;dbname=sample_db',$user,$password,$opt);
+    var_dump($dbh);
+
+} catch (PDOException $e){
+    echo $e->getMessage();
+    exit;
+}
